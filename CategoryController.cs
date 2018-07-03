b743e04c429a112cdd78b65537fda45adf7e5f82
Julian Leggs <jltiger599@gmail.com>
2018-07-03 00:08:05 -0500

    Initial commit

---
+++ CategoryController.cs
@@ -0,0 +1,52 @@
+﻿using CheeseMVC.Data;
+using CheeseMVC.Models;
+using CheeseMVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CheeseMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly CheeseDbContext context;
+
+        public CategoryController(CheeseDbContext dbContext)
+        {
+            context = dbContext;
+        }
+        public IActionResult Index()
+        {
+            List<CheeseCategory> allCat = context.Categories.ToList();
+            return View(allCat);
+        }
+        public IActionResult Add()
+        {
+            AddCategoryViewModel categoryViewModel = new AddCategoryViewModel();
+            return View(categoryViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddCategoryViewModel categoryViewModel)
+        {
+            if(ModelState.IsValid)
+            {
+                CheeseCategory newCategory = new CheeseCategory
+                {
+                    Name = categoryViewModel.Name
+                };
+                context.Categories.Add(newCategory);
+                context.SaveChanges();
+                return Redirect("/Category");
+            }
+            else
+            {
+                return View(categoryViewModel);
+            }
+        }
+    }
+}
+
+
+
+
